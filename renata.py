e6a651378eeae12a4c75b59d17314d728eb12ec8
Pratama Yoga Santosa <pratamayo16@gmail.com>
2020-04-17 11:00:44 +0700

    Renata !, Time to show

--- renata.py
+++ renata.py
@@ -10,16 +10,16 @@ start = 40
 # Sequential number of file
 idx = 0
 for filename in glob.glob('{}'.format(dirr)+'\*.jpg'): 
-    ```
+    '''
         If there is a restart of the sequential, please uncomment the code bellow !
-    ```
+    '''
     # if(idx == restart):
     #     idx = 0
     #     start+=1
 
-    ```
+    '''
        You can change the variance of rename as you want bellow !
-    ```
+    '''
     os.rename(r'{}'.format(filename),r'{}'.format(var)+'/'+var+'_{}'.format(start)+'.mp4_frame{}.jpg'.format(idx))
     idx+=1
 
